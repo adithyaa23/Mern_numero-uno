d3eaee24bb0e42ef5c30ef008532ac5b0cd9389f
adithyaa23 <62686245+adithyaa23@users.noreply.github.com>
2021-03-04 08:22:04 +0530

    Header section is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import Header from "./header";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* Header */}
+      <Header/>
+      {/* Swipe Section */}
+      {/* action */}
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import Forum from "@material-ui/icons/Forum"
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header__container">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon"/>
+      </IconButton>
+     
+      <img className="header_logo" src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo-1536x864.png" alt="logo_image" />
+
+      <IconButton>
+        <Forum fontSize="large" className="header_icon"/>
+      </IconButton>
+    </div>
+  )
+}
+export default Header;
